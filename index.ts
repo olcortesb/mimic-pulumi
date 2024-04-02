3b57231f7d7aed81675f1fe44b87ca11a8e16958
olcortesb <oscar.cortes@ackstorm.com>
2024-04-02 15:09:17 +0200

    fix name loggroup

--- index.ts
+++ index.ts
@@ -192,12 +192,12 @@ new aws.lambda.Permission("apigatewayResponse", {
 
 // CloudWatch
 
-const logGroupListen = new aws.cloudwatch.LogGroup("mimic_listen_pulumi", {
+const logGroupListen = new aws.cloudwatch.LogGroup("mimic-listen-pulumi", {
     name: `/aws/lambda/${lambda_listen.name}`,
     retentionInDays: 7, // keep logs for 7 days; customize retention as needed
 });
 
-const logGroupResponse = new aws.cloudwatch.LogGroup("mimic_response_pulumi", {
+const logGroupResponse = new aws.cloudwatch.LogGroup("mimic-response-pulumi", {
     name: `/aws/lambda/${lambda_response.name}`,
     retentionInDays: 7, // keep logs for 7 days; customize retention as needed
 });
