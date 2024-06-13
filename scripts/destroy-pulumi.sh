5b68b8baf282ece58939dd04799623fedc7f0b92
olcortesb <oscar.cortes@ackstorm.com>
2024-06-13 09:32:08 +0200

    feat: Add destroy stage

---
+++ scripts/destroy-pulumi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
+set -e -x
+
+# Add the pulumi CLI to the PATH
+export PATH=$PATH:$HOME/.pulumi/bin
+
+yarn install
+pulumi stack select dev
+pulumi config set aws:region eu-central-1
+# The following is just a sample config setting that the hypothetical pulumi
+# program needs.
+# Learn more about pulumi configuration at: https://www.pulumi.com/docs/concepts/config/
+# pulumi config set mysetting myvalue
+pulumi destroy --yes # this line will be the pulumi preview command in pulumi-preview.sh
